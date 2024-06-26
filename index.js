30fd14545ac59a9e4d8a68c09b773f2f9521a2d9
Siddharth Chauhan <siddharthchauhan847@gmail.com>
2024-06-26 23:54:43 +0530

    javascript added

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("siddharth");
+const inputBox=document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value===''){
+        alert("you must select something");
+
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+}
+    listContainer.addEventListener("click",function(e){
+        if(e.target.tagname==="LI"){
+            e.target.classList.toggle("checked");
+        }
+        else if(e.target.tagname==="SPAN"){
+            e.target.parentElement.remove();
+    }
+},false);
